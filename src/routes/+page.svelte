8b1dd2ddd703bd81543bdd3a513e08016850d9f8
ulysses-ck <ulisesapaza.280@gmail.com>
2024-03-17 10:30:24 -0300

    feat(timeout): define basic logic.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	// timer
 	let timer = 0;
 	let interval: number | null = null;
 
@@ -15,6 +16,40 @@
 	function resetChronometer() {
 		timer = 0;
 	}
+
+	// timeout
+	let seconds = 0;
+	let minutes = 0;
+	let hours = 0;
+	let isFinished = false;
+	let isStarted = false;
+
+	function startTimeout() {
+		isStarted = true;
+		const timeTotal = hours * 3600 + minutes * 60 + seconds;
+
+		// timeout
+		setTimeout(() => {
+			isFinished = true;
+			isStarted = false;
+		}, timeTotal * 1000);
+
+		// interval
+		const interval = setInterval(() => {
+			if (seconds > 0) {
+				seconds--;
+			} else if (minutes > 0) {
+				minutes--;
+				seconds = 59;
+			} else if (hours > 0) {
+				hours--;
+				minutes = 59;
+				seconds = 59;
+			} else {
+				clearInterval(interval);
+			}
+		}, 1000);
+	}
 </script>
 
 <svelte:head>
@@ -28,6 +63,24 @@
 	<button on:click={startChronometer}>Start</button>
 	<button on:click={stopChronometer}>Stop</button>
 	<button on:click={resetChronometer}>Reset</button>
+
+	<h2>Timeout</h2>
+	<p>Seconds: {seconds}</p>
+	<p>Minutes: {minutes}</p>
+	<p>Hours: {hours}</p>
+	<label for="seconds">
+		<span>Seconds:</span>
+		<input type="number" bind:value={seconds} name="seconds" id="seconds" disabled={isStarted} />
+	</label>
+	<label for="minutes">
+		<span>Minutes:</span>
+		<input type="number" bind:value={minutes} name="minutes" id="minutes" disabled={isStarted} />
+	</label>
+	<label for="hours">
+		<span>Hours:</span>
+		<input type="number" bind:value={hours} name="hours" id="hours" disabled={isStarted} />
+	</label>
+	<button on:click={startTimeout}>Start Timeout</button>
 </section>
 
 <style>
