1c9696abe8ae0cf8c56469cd604a0b576482e3cb
ulysses-ck <ulisesapaza.280@gmail.com>
2024-03-16 23:43:09 -0300

    fix(general): remove almost boilerplate.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,43 +11,11 @@
 	</main>
 
 	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+		<p>
+			Made with ❤️ by <a href="https://svelte.dev">Svelte</a> and
+			<a href="https://kit.svelte.dev">SvelteKit</a>
+		</p>
 	</footer>
 </div>
 
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
-</style>
+<style></style>
